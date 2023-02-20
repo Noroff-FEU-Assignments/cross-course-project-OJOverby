89226ff5a243d108a09897d66c8ee779850ad7db
Ole Jørgen <quezt@outlook.com>
2023-02-20 21:03:44 +0100

    Uploaded all pages
    Uploaded all pages from beta to main

---
+++ js/gamehub.js
@@ -0,0 +1,20 @@
+var dimmed = document.querySelector('.dimmed')
+var checkbox = document.querySelector('#menu-checkbox');
+var menu = document.querySelector('#navmenu');
+var screenheight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+var racingGames = document.querySelectorAll('.racingSelector');
+
+
+
+dimmed.addEventListener('click', function() {
+    checkbox.checked = false;
+});
+
+console.log(dimmed.style.height = screenheight+('px'));
+
+dimmed.style.height = screenheight+('px');
+
+console.log(menu)
+
+menu.style.height = screenheight+('px');
+
---
+++ js/gamesdb.js
@@ -0,0 +1,85 @@
+document.write(`
+    
+<div class="gamecontainer">
+
+<section class="gamescard RPGgames">
+  <img src="images/AssassinCover.jpg" alt="Assassin game cover" class="cover-card">
+  <h2>ASSASSIN</h2>
+  <h2>$49.99</h2>
+  <a href="#"><div class="addgamesbutton-card CTA">ADD TO CART</div></a>
+  <a href="racing.html"><div class="readmorebutton-card">READ MORE</div></a>
+</section>
+
+<section class="gamescard actiongames">
+  <img src="images/BlackCover.jpg" alt="Black game cover" class="cover-card">
+  <h2>BLACK</h2>
+  <h2>$49.99</h2>
+  <a href="cart.html"><div class="addgamesbutton-card CTA">ADD TO CART</div></a>
+  <a href="racing.html"><div class="readmorebutton-card">READ MORE</div></a>
+</section>
+
+<section class="gamescard sportsgames">
+  <img src="images/BoxerCover.jpg" alt="Boxer game cover" class="cover-card">
+  <h2>BOXER</h2>
+  <h2>$49.99</h2>
+  <a href="cart.html"><div class="addgamesbutton-card CTA">ADD TO CART</div></a>
+  <a href="racing.html"><div class="readmorebutton-card">READ MORE</div></a>
+</section>
+
+<section class="gamescard actiongames">
+  <img src="images/CyberpunkCover.jpg" alt="Cyberpunk game cover" class="cover-card">
+  <h2>CYBERPUNK</h2>
+  <h2>$49.99</h2>
+  <a href="cart.html"><div class="addgamesbutton-card CTA">ADD TO CART</div></a>
+  <a href="racing.html"><div class="readmorebutton-card">READ MORE</div></a>
+</section>
+
+<section class="gamescard RPGgames">
+  <img src="images/ForgeOfLegendsCover.jpg" alt="Forge of Legends game cover" class="cover-card">
+  <h2>FORGE OF LEGENDS</h2>
+  <h2>$49.99</h2>
+  <a href="cart.html"><div class="addgamesbutton-card CTA">ADD TO CART</div></a>
+  <a href="racing.html"><div class="readmorebutton-card">READ MORE</div></a>
+</section>
+
+<section class="gamescard actiongames">
+  <img src="images/FuriousCover.jpg" alt="Furious game cover" class="cover-card">
+  <h2>FURIOUS</h2>
+  <h2>$49.99</h2>
+  <a href="cart.html"><div class="addgamesbutton-card CTA">ADD TO CART</div></a>
+  <a href="racing.html"><div class="readmorebutton-card">READ MORE</div></a>
+</section>
+
+<section class="gamescard sportsgames">
+  <img src="images/PingPongCover.jpg" alt="PING PONG game cover" class="cover-card">
+  <h2>PING PONG</h2>
+  <h2>$49.99</h2>
+  <a href="cart.html"><div class="addgamesbutton-card CTA">ADD TO CART</div></a>
+  <a href="racing.html"><div class="readmorebutton-card">READ MORE</div></a>
+</section>
+
+<section class="gamescard racinggames">
+  <img src="images/RacingCover.jpg" alt="Racing game cover" class="cover-card">
+  <h2>RACING</h2>
+  <h2>$49.99</h2>
+  <a href="cart.html"><div class="addgamesbutton-card CTA">ADD TO CART</div></a>
+  <a href="racing.html"><div class="readmorebutton-card">READ MORE</div></a>
+</section>
+<section class="gamescard actiongames">
+  <img src="images/SpaceWarCover.jpg" alt="Space War game cover" class="cover-card">
+  <h2>SPACE WAR</h2>
+  <h2>$49.99</h2>
+  <a href="cart.html"><div class="addgamesbutton-card CTA">ADD TO CART</div></a>
+  <a href="racing.html"><div class="readmorebutton-card">READ MORE</div></a>
+</section>
+<section class="gamescard familygames">
+  <img src="images/SuperDuperCover.jpg" alt="Super Duper game cover" class="cover-card">
+  <h2>SUPER DUPER</h2>
+  <h2>$49.99</h2>
+  <a href="cart.html"><div class="addgamesbutton-card CTA">ADD TO CART</div></a>
+  <a href="racing.html"><div class="readmorebutton-card">READ MORE</div></a>
+</section>
+
+</div>
+
+`);
